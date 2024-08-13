169e14f06acdbe58310afe4f95006be3476de187
arkim <arkim@lfit.co.kr>
2024-08-13 11:48:05 +0900

    수정

--- src/component/Box.js
+++ src/component/Box.js
@@ -6,7 +6,7 @@ const Box = (props) => {
       <div className={`box ${props.result}`}>
         <div>{props.title}</div>
         <div className="img-box">
-          <img className="image" src={props.item && props.item.url} />      
+          <img className="image" src={props.item && props.item.url} alt={props.title}/>
         </div>
         <div>{props.item && props.item.name}</div>
         <div className='result'>
